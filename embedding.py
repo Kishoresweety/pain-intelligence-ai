af702286fec70ebfc6f5fd128d864d14402d4baa
Kishore <109215974+Kishoresweety@users.noreply.github.com>
2026-04-02 23:51:37 +0530

    Update embedding.py

--- embedding.py
+++ embedding.py
@@ -1,3 +1,16 @@
+from openai import OpenAI
+import os
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+def get_embedding(text):
+    response = client.embeddings.create(
+        model="text-embedding-3-small",
+        input=text
+    )
+    return response.data[0].embedding
+
+''' 
 import os
 from openai import OpenAI
 
@@ -5,8 +18,9 @@ client = OpenAI()
 
 client.api_key = os.getenv("OPENAI_API_KEY")
 def get_embedding(text):
-    response = client.Embedding.create(
+    response = client.embeddings.create(
         model="text-embedding-3-small",
         input=text
     )
     return response['data'][0]['embedding']
+'''
