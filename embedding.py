c47e4c0391347e2d2e8e3795d4ca9fbbda99a9ab
Kishore <109215974+Kishoresweety@users.noreply.github.com>
2026-04-02 20:23:01 +0530

    Create embedding.py

---
+++ embedding.py
@@ -0,0 +1,10 @@
+import os
+import openai
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+def get_embedding(text):
+    response = openai.Embedding.create(
+        model="text-embedding-3-small",
+        input=text
+    )
+    return response['data'][0]['embedding']
